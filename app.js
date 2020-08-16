dc4e07e8aa44c91c2d5bab1c184a55b886e4ae8a
Pete O'Shea <mail@peteoshea.co.uk>
2020-08-16 20:09:52 +0100

    Add register end point
    Allow access to the data from the request by processing the urlencoded fields.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('signup'));
 app.listen(3001, () => console.log('Server ready'));
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+});
