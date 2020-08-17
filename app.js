1fc917f66aa878457b0f7de1955d30e918652005
Pete O'Shea <mail@peteoshea.co.uk>
2020-08-17 18:33:15 +0100

    Add login endpoint

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const passport = require('passport');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 app.set('view engine', 'pug');
 app.use(express.urlencoded({ extended: true }));
@@ -27,6 +27,34 @@ passport.deserializeUser((email, done) => {
   });
 });
 
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async function (email, password, done) {
+      if (!email || !password) {
+        done('Email and password required', null);
+        return;
+      }
+
+      const user = await User.findOne({ where: { email: email } });
+      if (!user) {
+        done('User not found', null);
+        return;
+      }
+
+      const valid = await user.isPasswordValid(password);
+      if (!valid) {
+        done('Email and password do not match', null);
+        return;
+      }
+
+      done(null, user);
+    }
+  )
+);
 app.use(
   session({
     secret: 'g56434g4151r2rsbdfgdytjaedm',
@@ -62,8 +90,20 @@ app.post('/register', async (req, res) => {
     res.render('error', { message });
   }
 });
+app.get('/login', (req, res) => (req.session.passport ? res.render('index') : res.render('login')));
+app.post('/login', async (req, res) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return res.render('error', { message: err });
+    if (!user) return res.render('error', { message: 'No user found matching credentials' });
+
+    req.login(user, (err) => {
+      if (err) return res.render('error', { message: err });
+      return res.redirect('/');
+    });
+  })(req, res);
+});
 app.get('/logout', async (req, res) => {
   req.logout();
   req.session.destroy();
-  return res.redirect('/');
+  return res.redirect('/login');
 });
